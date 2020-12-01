90536036267973c32d5dbea2a41866f412c9d60d
Michelle Jiang <mj3733@students.necsd.net>
2020-12-01 15:19:36 +0000

    did 1 and 2

--- Prob2.java
+++ Prob2.java
@@ -2,9 +2,19 @@ import java.util.Scanner;
 
 public class Prob2  {
   public static void main(String[] args){
-    
+  Scanner scan = new Scanner(System.in); 
+ System.out.println("Enter four decimals: "); 
+
+ double num1 = scan.nextDouble();
+ double num2 = scan.nextDouble();
+ double num3 = scan.nextDouble();
+ double num4 = scan.nextDouble();
+
+ double all = (num1 + num2 + num3 + num4)/4;
+ if (all >= 89.5) 
+ System.out.println("ABOVE AVERAGE");
 
 
-    
+  
   }
 }
