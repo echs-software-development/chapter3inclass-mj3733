153e7f84c83c5e2b159cfa18e7a62d6963abba46
Michelle Jiang <mj3733@students.necsd.net>
2020-12-03 15:21:07 +0000

    did 3 and 4

--- Prob4.java
+++ Prob4.java
@@ -3,8 +3,18 @@ import java.util.Scanner;
 public class Prob4  {
   public static void main(String[] args){
     
+  Scanner scan = new Scanner(System.in);
+  System.out.println("What is the temperature?");
 
-
+  double temp = scan.nextDouble();
+  if (temp < 99 || > 102) {  
+   System.out.println("WARNING");
+  }
+  else {  
+   System.out.println("Temperature is OK");
+  }
+ 
+   
     
   }
 }
